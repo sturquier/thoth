4eb65cc37cda131d913873b6756b54f7d16c314a
sturquier <turquiersimon@hotmail.fr>
2020-06-07 22:47:59 +0200

    Fix article creation date format for jest snapshots

--- src/app/pages/Home/Home.test.tsx
+++ src/app/pages/Home/Home.test.tsx
@@ -21,8 +21,8 @@ describe('Home', () => {
   const props = {
     loading: false,
     articles: [
-      { id: 1, title: 'First article', created_at: moment('2020-12-31T00:00:00'), url: 'https://www.foo.com/article' },
-      { id: 2, title: 'Second article', description: 'Lorem Ipsum', created_at: moment('2020-12-30T00:00:00'), url: 'https://www.bar.com/article', image: 'https://www.bar.com/article-image.jpeg' }
+      { id: 1, title: 'First article', created_at: moment('2020-12-31T00:00:00').format(), url: 'https://www.foo.com/article' },
+      { id: 2, title: 'Second article', description: 'Lorem Ipsum', created_at: moment('2020-12-30T00:00:00').format(), url: 'https://www.bar.com/article', image: 'https://www.bar.com/article-image.jpeg' }
     ],
     onFetchArticles: jest.fn()
   }
