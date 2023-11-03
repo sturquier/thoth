e07a6b04afa5ee0ffe0abf968d6a6cebab0e694b
sturquier <turquiersimon@hotmail.fr>
2023-11-03 17:46:15 +0100

    Handle tabs in profile screen & render favorite articles

--- lib/screens/profile/profile.dart
+++ lib/screens/profile/profile.dart
@@ -1,19 +1,120 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:thoth/config/authentication.dart';
+import 'package:thoth/models/article.dart';
+import 'package:thoth/provider/articles.dart';
+import 'package:thoth/services/favorites.dart';
+import 'package:thoth/widgets/card/card.dart';
 
-class ProfileScreen extends StatefulWidget {
+class ProfileScreen extends ConsumerStatefulWidget {
   const ProfileScreen({super.key});
 
   @override
-  State<ProfileScreen> createState() => _ProfileScreenState();
+  ConsumerState<ProfileScreen> createState() => _ProfileScreenState();
 }
 
-class _ProfileScreenState extends State<ProfileScreen> {
+class _ProfileScreenState extends ConsumerState<ProfileScreen>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _tabController = TabController(length: 2, vsync: this);
+    _tabController.addListener(_handleTabSelection);
+  }
+
+  @override
+  void dispose() {
+    _tabController.removeListener(_handleTabSelection);
+    _tabController.dispose();
+
+    super.dispose();
+  }
+
+  void _handleTabSelection() {
+    if (_tabController.indexIsChanging) {
+      setState(() {});
+    }
+  }
+
   void _logout() {
     authenticationInstance
         .signOut()
         .then((void _) => GoRouter.of(context).go('/login'));
+  }
+
+  void _toggleFavorite(Article article) async {
+    await toggleFavorite(article);
+    await ref.read(articlesProvider.notifier).fetchArticlesValues();
+  }
+
+  Widget _buildInformationsTab() {
+    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+      const Text(
+        'Mes informations personnelles',
+        style: TextStyle(fontSize: 18),
+      ),
+      const SizedBox(
+        height: 30,
+      ),
+      const Text(
+        'Adresse email',
+        style: TextStyle(fontSize: 16),
+      ),
+      const SizedBox(
+        height: 5,
+      ),
+      Text(getCurrentUser()!.email!)
+    ]);
+  }
+
+  Widget _buildFavoritesTab() {
+    final AsyncValue<List<Article>> articlesValue = ref.watch(articlesProvider);
+
+    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+      const Text(
+        'Mes articles favoris',
+        style: TextStyle(fontSize: 18),
+      ),
+      const SizedBox(
+        height: 30,
+      ),
+      Expanded(
+        child: articlesValue.when(
+            loading: () => const Center(child: CircularProgressIndicator()),
+            error: (Object error, StackTrace stackTrace) => const Center(
+                  child: Text(
+                    "Une erreur s'est produite",
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+            data: (List<Article> articles) {
+              final favorites = articles
+                  .where((Article article) => article.isFavorite)
+                  .toList();
+              return favorites.isEmpty
+                  ? const Center(
+                      child: Text(
+                      "Aucun favori n'a été trouvé",
+                      style: TextStyle(fontSize: 18),
+                    ))
+                  : ListView.builder(
+                      itemCount: favorites.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        Article article = favorites[index];
+
+                        return ArticleCardWidget(
+                            article: article,
+                            toggleFavoriteCallback: () =>
+                                _toggleFavorite(article));
+                      },
+                    );
+            }),
+      )
+    ]);
   }
 
   @override
@@ -26,42 +127,33 @@ class _ProfileScreenState extends State<ProfileScreen> {
         Container(
             padding: const EdgeInsets.symmetric(horizontal: 15),
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
+                TabBar(controller: _tabController, tabs: const [
+                  Tab(text: 'Informations'),
+                  Tab(text: 'Favoris')
+                ]),
                 const SizedBox(
                   height: 20,
                 ),
-                const Text(
-                  'Mes informations personnelles',
-                  style: TextStyle(fontSize: 18),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                const Divider(),
-                const SizedBox(
-                  height: 20,
-                ),
-                const Text(
-                  'Adresse email',
-                  style: TextStyle(fontSize: 16),
-                ),
-                const SizedBox(
-                  height: 5,
-                ),
-                Text(getCurrentUser()!.email!),
+                Expanded(
+                    child: TabBarView(controller: _tabController, children: [
+                  _buildInformationsTab(),
+                  _buildFavoritesTab()
+                ])),
               ],
             )),
-        Positioned(
-            bottom: 15,
-            left: 15,
-            right: 15,
-            child: ElevatedButton(
-                onPressed: _logout,
-                child: const Text(
-                  'Se déconnecter',
-                  style: TextStyle(fontSize: 16),
-                )))
+        if (_tabController.index == 0) ...[
+          Positioned(
+              bottom: 15,
+              left: 15,
+              right: 15,
+              child: ElevatedButton(
+                  onPressed: _logout,
+                  child: const Text(
+                    'Se déconnecter',
+                    style: TextStyle(fontSize: 16),
+                  )))
+        ]
       ]),
     );
   }
